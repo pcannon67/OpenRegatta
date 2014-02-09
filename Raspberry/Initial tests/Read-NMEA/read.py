0f186fd1e492a2c7150affd0844b933591c7e9ff
David Dieffenthaler <ddieffen@gmail.com>
2014-02-09 16:22:18 -0600

    Update read.py

--- Raspberry/Initial tests/Read-NMEA/read.py
+++ Raspberry/Initial tests/Read-NMEA/read.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 #This scripts opens the RaspberryPi default serial port and reads data from it's buffer
 #The device is set to read data at 4800bps 8bit data 1 bit stop no parity, which is 
 #equivalent as the NMEA input usually found on marine devices.
@@ -5,7 +7,6 @@
 #The program reads the data and outputs it to the console so the user executing this 
 #script is able to monitor the data that is coming though the serial port
 
-#!/usr/bin/python
 import serial
 ser = serial.Serial('/dev/ttyAMA0', 4800, timeout=1)
 ser.open()
