7ed73da6b5c3965b04f28b09cb8055444bd61358
David Dieffenthaler <ddieffen@gmail.com>
2014-04-07 23:18:51 -0500

    Added visual studio solution to stream nmea to COM port for simulation.
    Touched the layouts but nothing dramatic was done in the android app.
    Now that I have a solid testing framework I'll get more actively on the
    application development

---
+++ Raspberry/SerialPortSendText/SerialPortSendText/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace SerialPortSendText
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(231, 192);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.textBox1.Location = new System.Drawing.Point(24, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(532, 174);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(21, 239);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(535, 69);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Text send though serial port";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(568, 317);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Raspberry/SerialPortSendText/SerialPortSendText/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+namespace SerialPortSendText
+{
+    public partial class Form1 : Form
+    {
+        static SerialPort _serialPort;
+        static BackgroundWorker _worker;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Create a new SerialPort object with default settings.
+            _serialPort = new SerialPort();
+
+            // Allow the user to set the appropriate properties.
+            _serialPort.PortName = "COM5";
+            _serialPort.BaudRate = 4800;
+            _serialPort.Parity = Parity.None;
+            _serialPort.DataBits = 8;
+            _serialPort.StopBits = StopBits.One;
+            _serialPort.Handshake = Handshake.None;
+
+            // Set the read/write timeouts
+            _serialPort.ReadTimeout = 500;
+            _serialPort.WriteTimeout = 500;
+
+            _serialPort.Open();
+
+            _worker = new BackgroundWorker();
+            _worker.DoWork += new DoWorkEventHandler(_worker_DoWork);
+        }
+
+        void _worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if (_serialPort != null
+                && _serialPort.IsOpen)
+            {
+                String[] split = this.textBox1.Text.Split('\n');
+                String toSend = "";
+                foreach (String str in split)
+                {
+                    toSend = str+"\n";
+                    _serialPort.Write(toSend);
+                    this.setLabelText(toSend);
+                    if (toSend.StartsWith("$IIVTG"))
+                        Thread.Sleep(2500);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _worker.RunWorkerAsync();
+        }
+
+        private void setLabelText(string p)
+        {
+            if(this.label1.InvokeRequired){
+                this.label1.Invoke((MethodInvoker)delegate {
+                    setLabelText(p); 
+                });
+                return;
+            }
+            this.label1.Text = p;    
+        }
+    }
+
+}
